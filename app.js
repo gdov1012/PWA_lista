fbf4e26ce2aa408cdf769a4a3f25e848b41feb08
gdov1012 <valerogiovanna7@gmail.com>
2025-08-30 19:35:09 -0300

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     //registro do sw
-    navigator.serviceWorker.register('sw.js')
+    navigator.serviceWorker.register('/PWA_lista/sw.js')
         .then(() => console.log('Service Worker registrado com sucesso!'))
         .catch(err => console.log('Erro ao registrar Service Worker:', err));
 }
